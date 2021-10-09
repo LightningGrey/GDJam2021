0f3dccc6f2655ac121c4bf96d01dd96d10a0ddf6
Hunter <hunt3rchu@gmail.om>
2021-10-09 18:34:50 -0400

    updated player animations

--- Assets/Scripts/Player + Controls/PlayerControls.cs
+++ Assets/Scripts/Player + Controls/PlayerControls.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
 {
@@ -8,9 +10,12 @@ public class PlayerControls : MonoBehaviour
 
     [Header("References")]
     [SerializeField] private Rigidbody2D _rb;
+    [SerializeField] private Animator _animator;
 
     [Header("Variables")] 
-    [SerializeField] private float _speed = 4.0f;
+    [SerializeField] private float _speed = 0.5f;
+
+    private Vector2 _moveInput = Vector2.zero;
 
 
     // Start is called before the first frame update
@@ -27,15 +32,48 @@ public class PlayerControls : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector2 moveInput = _playerKbMove.Keyboard.Move.ReadValue<Vector2>();
-        _rb.velocity = moveInput * _speed;
+        _moveInput = Vector2.ClampMagnitude(_playerKbMove.Keyboard.Move.ReadValue<Vector2>(), 1.0f);
+        _rb.velocity = _moveInput * _speed;
 
+    }
+
+    void Update()
+    {
+        Animate();
     }
 
     void Interact()
     {
         InteractionManager.Instance.CallEvent();
     }
+
+    void Animate()
+    {
+        if (Keyboard.current.upArrowKey.wasPressedThisFrame || 
+            Keyboard.current.wKey.wasPressedThisFrame)
+        {
+            _animator.SetInteger("Direction", 1);
+        }
+        else if (Keyboard.current.downArrowKey.wasPressedThisFrame ||
+                 Keyboard.current.sKey.wasPressedThisFrame)
+        {
+            _animator.SetInteger("Direction", 0);
+        }
+        else if (Keyboard.current.leftArrowKey.wasPressedThisFrame ||
+                 Keyboard.current.aKey.wasPressedThisFrame)
+        {
+            _animator.SetInteger("Direction", 3);
+        }
+        else if (Keyboard.current.rightArrowKey.wasPressedThisFrame ||
+                 Keyboard.current.dKey.wasPressedThisFrame)
+        {
+            _animator.SetInteger("Direction", 2);
+        }
+
+
+        _animator.SetBool("IsMoving", _moveInput != Vector2.zero);
+    }
+
 
     void OnEnable()
     {
